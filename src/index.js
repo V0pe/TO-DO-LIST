e295a01f89c5671f154fad9e3a7f3287d26d78e6
ARANY <v.aderibigbe96@gmail.com>
2022-04-27 06:52:44 -0400

    add the markup for landing page

--- src/index.js
+++ src/index.js
@@ -2,13 +2,3 @@ import './style.css';
 import _ from 'lodash';
 
 
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
