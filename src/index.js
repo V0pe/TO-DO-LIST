37cde3b95f106a2d461d5d3b510fa882d16a30d8
ARANY <v.aderibigbe96@gmail.com>
2022-04-27 07:45:30 -0400

    fix linters error

--- src/index.js
+++ src/index.js
@@ -1,46 +1,37 @@
-
 import Enter from './enter.svg';
 import Refresh from './refresh.svg';
 import Option from './dots.svg';
 import './style.css';
 
-let taskList = document.querySelector('.tasks');
-let header = document.querySelector('.header');
-let add = document.querySelector('.add');
+const taskList = document.querySelector('.tasks');
+const header = document.querySelector('.header');
+const add = document.querySelector('.add');
 const tasks = [
-    {
-        description :'Iron clothes',
-        completed:false,
-        index: 0,
-    },
-    {
-        description :'watch ball',
-        completed:false,
-        index: 1,
-    }
-]
+  {
+    description: 'Iron clothes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'watch ball',
+    completed: false,
+    index: 1,
+  },
+];
 
-
-for (let task of tasks){
-    let templateHTML = `<div class="task" id="${task.index}">
+tasks.map((task) => {
+  const templateHTML = `<div class="task" id="${task.index}">
     <input type="checkbox">
     <p>${task.description}</p>
     <img src="${Option}" alt="">
-</div>`
-    taskList.insertAdjacentHTML("beforeend", templateHTML);
-}
+</div>`;
+  taskList.insertAdjacentHTML('beforeend', templateHTML);
+  return true;
+});
 
 const refreshIcon = new Image();
-  refreshIcon.src = Refresh;
-  header.appendChild(refreshIcon);
-  const enterIcon = new Image();
-  enterIcon.src = Enter;
-  add.appendChild(enterIcon);
-
-
-
-alert(("first time"));
-
-
-
-
+refreshIcon.src = Refresh;
+header.appendChild(refreshIcon);
+const enterIcon = new Image();
+enterIcon.src = Enter;
+add.appendChild(enterIcon);
