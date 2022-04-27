b7f6c5b518e2a6977a1c8761f376deb948fc1a37
ARANY <v.aderibigbe96@gmail.com>
2022-04-27 07:37:24 -0400

    added the javascript file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import Enter from './enter.svg';
 import Refresh from './refresh.svg';
 import Option from './dots.svg';
 import './style.css';
-import _ from 'lodash';
 
 let taskList = document.querySelector('.tasks');
+let header = document.querySelector('.header');
+let add = document.querySelector('.add');
 const tasks = [
     {
         description :'Iron clothes',
@@ -16,17 +17,30 @@ const tasks = [
         description :'watch ball',
         completed:false,
         index: 1,
-    },
+    }
 ]
+
 
 for (let task of tasks){
     let templateHTML = `<div class="task" id="${task.index}">
     <input type="checkbox">
     <p>${task.description}</p>
-    <img src="./dots.svg" alt="">
+    <img src="${Option}" alt="">
 </div>`
     taskList.insertAdjacentHTML("beforeend", templateHTML);
 }
+
+const refreshIcon = new Image();
+  refreshIcon.src = Refresh;
+  header.appendChild(refreshIcon);
+  const enterIcon = new Image();
+  enterIcon.src = Enter;
+  add.appendChild(enterIcon);
+
+
+
+alert(("first time"));
+
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,13 @@ module.exports = {
       },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: './src/index.html'
         }),
       ],
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
+      
       clean:true,
     },
     module: {
