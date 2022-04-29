dc28c5c20a1d6206c037c305b828a22b5f2945f1
ARANY <v.aderibigbe96@gmail.com>
2022-04-29 09:48:15 -0400

    added the image for the edit mode

--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import './style.css';
 import Enter from './enter.svg';
 import Refresh from './refresh.svg';
 import Option from './dots.svg';
+import Trash from './trash.svg';
 
-const taskList = document.querySelector('.tasks');
 const header = document.querySelector('.header');
 const add = document.querySelector('.add');
-const tasks = [
-  {
-    description: 'Iron clothes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'watch ball',
-    completed: false,
-    index: 1,
-  },
-];
 
-tasks.map((task) => {
-  const templateHTML = `<div class="task" id="${task.index}">
-    <input type="checkbox">
-    <p>${task.description}</p>
-    <img src="${Option}" alt="">
-</div>`;
-  taskList.insertAdjacentHTML('beforeend', templateHTML);
-  return true;
-});
 
 const refreshIcon = new Image();
 refreshIcon.src = Refresh;
@@ -35,3 +14,6 @@ header.appendChild(refreshIcon);
 const enterIcon = new Image();
 enterIcon.src = Enter;
 add.appendChild(enterIcon);
+
+
+
