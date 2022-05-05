8b9243bceab8a717b52d5170228b7c3ee63c6bab
ARANY <v.aderibigbe96@gmail.com>
2022-05-05 03:39:42 -0400

    fix deploy error

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
     publicPath: '/TO-DO-LIST/',
+    clean: true,
   },
   module: {
     rules: [
